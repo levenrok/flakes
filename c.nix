6b885f7563719edeba29582606e217380b4bce4c
Leven Rochana <levenrok@proton.me>
2025-11-25 22:15:31 +0530

    add flake for c

---
+++ c.nix
@@ -0,0 +1,39 @@
+{
+  description = "Flake for C";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "c";
+
+        nativeBuildInputs = with pkgs;[
+          clang
+          clang-tools
+
+          gnumake
+          cmake
+          cmake-language-server
+          gdb
+
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          zlib
+        ];
+
+        env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.zlib
+        ];
+      };
+    };
+}
