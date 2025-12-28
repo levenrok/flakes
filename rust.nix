a132027337d71b35a9e22ca5d407d073bf2e223f
Leven Rochana <levenrok@proton.me>
2025-12-29 03:13:28 +0530

    add flake for rust

---
+++ rust.nix
@@ -0,0 +1,33 @@
+{
+  description = "Flake for Rust Development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "rust";
+
+        buildInputs = with pkgs ;[
+          cargo
+          rustc
+
+          rust-analyzer
+          rustfmt
+          clippy
+        ];
+
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+        shellHook = ''
+          echo -e "\033[0;32mDone!\033[0m"
+        '';
+      };
+    };
+}
