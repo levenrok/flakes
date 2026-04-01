bdaa940c05de8d7e7db5d2182a5c09f323354a17
Leven Rochana <levenrok@proton.me>
2026-04-01 17:53:55 +0530

    update rust flake to use fenix for toolchain management

--- rust.nix
+++ rust.nix
@@ -3,35 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
     flake-utils.url = "github:numtide/flake-utils";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
+    { self
+    , nixpkgs
+    , flake-utils
+    , fenix
+    ,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        toolchain = (fenix.packages.${system}.toolchainOf {
+          channel = "nightly";
+          date = "2026-03-21";
+          sha256 = "sha256-rboGKQLH4eDuiY01SINOqmXUFUNr9F4awoFZGzib17o=";
+        }).withComponents [
+          "cargo"
+          "clippy"
+          "rustc"
+          "rustfmt"
+          "rust-analyzer"
+          "rust-src"
+          "llvm-tools-preview"
+          "rustc-codegen-cranelift-preview"
+        ];
       in
       {
         devShells.default = pkgs.mkShell {
           name = "rust";
 
-          nativeBuildInputs = with pkgs; [
-            cargo
-            rustc
-
-            rust-analyzer
-            rustfmt
-            clippy
+          nativeBuildInputs = [
+            toolchain
+            pkgs.mold
           ];
 
-          env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          env.RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
 
           shellHook = ''
             echo -e "\033[0;32mDone!\033[0m"
